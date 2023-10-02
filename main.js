18e1af195d5097eba4f225df27849b9a466f89a1
Joaquin Alonso <acoalonso11@gmail.com>
2023-10-02 15:39:20 -0300

    limpieza de codigo y titulo html

--- main.js
+++ main.js
@@ -1,123 +1,3 @@
-//comentario de linea
-
-/*comentario 
-de
-bloque
-*/
-
-
-/* let numeroUno = 3;
-let numeroDos = 5;
-let resultadoSuma = numeroUno + numeroDos;
-
-console.log(resultadoSuma);
-*/
-/*
-let nombre = "Juan";
-let saludo = "Hola";
-
-let saludoCompleto = saludo + " " + nombre;
-
-console.log(saludoCompleto);
-
-
-let nombre = prompt("ingrese su nombre");
-alert("gracias"+ " " + nombre);
-
-*/
-
-// let numeroUno = parseInt(prompt("Ingresa el numero 1"));
-// let numeroDos = parseInt(prompt("Ingresa el numero 2"));
-
-// alert(numeroUno + numeroDos);
-
-
-// let edad = parseInt(prompt("Ingresa aqui tu edad"));
-
-// if (edad >= 10 && edad <= 50){
-//     alert("Puedes utilizar nuestro producto");
-// } else {
-//     alert("Rango de edad no soportado");
-// }
-
-
-// let saludo = prompt("Que dices al llegar a algun lugar?");
-
-// if (saludo.toLowerCase() === "hola") {
-//     alert("Bienvenido, puedes pasar!");
-// } else {
-//     alert("Aprende modales primero");
-// }
-
-// let dinero = prompt("Cual es tu gasto diario en pesos?");
-
-// if (dinero >= 1000) {
-//     alert("Debes gastar menos")
-// }else {
-//     alert("Estas dentro del presupuesto")
-// }
-
-// let edad = parseInt(prompt("cuantos anos tenes?"));
-// if (edad >= 18 && edad <= 149) {
-//     alert("Bienvenido")
-
-// }else {
-//     alert("Edad no valida");
-// }
-
-
-
-// let cagadas = prompt("Cuantas veces defecas al dia(Escribe `ESC` para salir)");
-// let numeroCagadas;
-
-// while (cagadas !== "ESC" && cagadas !== null) {
-//     numeroCagadas = parseInt(cagadas);
-//     switch (numeroCagadas) {
-//         case 1:
-//         alert("Intenta ir mas");
-//         break;
-
-//         case 2:
-//             alert("Esta muy bien");
-//             break;
-        
-//         case 3:
-//             alert("Esta muy bien");
-//             break;
-
-//         case 4:
-//             alert("un poco mucho");
-//             break;
-        
-//         case 5:
-//             alert("Estas en el limite, controlate");
-//             break;
-
-//         default:
-//             alert("Sos humano?");
-//             break;
-//     }
-//     cagadas = prompt("Cuantas veces defecas al dia(Escribe `ESC` para salir)");
-// }
-
-
-// let marca = prompt("Ingrese la marca del vehiculo");
-// let modelo = prompt("Ingrese el modelo del vehiculo");
-// let cilindrada = prompt("Ingrese la cilindrada del vehiculo");
-// let precio = prompt("Ingrese el precio del vehiculo");
-
-// const automovil = {
-//     marca,
-//     modelo,
-//     cilindrada,
-//     precio,
-// };
-
-// alert(`Marca: ${automovil.marca} Modelo: ${automovil.modelo} Cilindrada: ${automovil.cilindrada} Precio: ${automovil.precio}`);
-
-
-
-
 
 // Función para calcular el precio del seguro
 function calcularSeguro(edad, categoria) {
@@ -143,7 +23,7 @@ function calcularSeguro(edad, categoria) {
     if (edad <= 25 >= 18) {
       precioBase *= 1.2; // Incremento del 20%
     } else if (edad < 18) {
-        alert("Debes ser mayor para contratar un seguro");
+        alert("Debes ser mayor de edad para contratar un seguro");
         return 0;
     }
 
@@ -163,7 +43,7 @@ function calcularSeguro(edad, categoria) {
     if (precioSeguro > 0) {
         alert(`El precio del seguro es: $${precioSeguro} anuales`);
         alert(`Gracias por utilizar nuestro cotizador de seguros`);
-      break; // Salir del ciclo si se obtiene un resultado válido
+      break; 
     }
   }
   
